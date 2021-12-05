d137e173e2d47c0da4f9bca3e6ced036650dc571
Muss86 <florin.gruianu@gmail.com>
2021-12-05 03:03:32 +0200

    carousel product-tile-list

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,16 @@
 import './../scss/index.scss';
 import Glide from '@glidejs/glide';
 
-new Glide('.glide', {
+new Glide('.home-banner.glide', {
   type: 'carousel',
+}).mount();
+
+new Glide('.product-tile-list.glide', {
+  type: 'carousel',
+  perView: 3,
+  breakpoints: {
+    991: {
+      perView: 1,
+    },
+  },
 }).mount();
