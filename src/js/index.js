a55fc91d36464a627ac82b99feb827f8b08a3695
Muss86 <florin.gruianu@gmail.com>
2021-12-10 01:14:15 +0200

    reparat carousel din catalog preview

--- src/js/index.js
+++ src/js/index.js
@@ -14,3 +14,13 @@ new Glide('.product-tile-list.glide', {
     },
   },
 }).mount();
+
+new Glide('.home-catalog-preview:nth-of-type(2) .product-tile-list.glide', {
+  type: 'carousel',
+  perView: 3,
+  breakpoints: {
+    991: {
+      perView: 1,
+    },
+  },
+}).mount();
